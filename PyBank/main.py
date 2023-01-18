447fdc5b611fcb1c06e7d16d9e3ea6a54c36d7a4
knnthchng <neth@Kenneths-Mini.home>
2023-01-17 21:15:52 -0800

    commit 3.5

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,12 @@ with open(filepath, 'r') as csvfile:
         months.append(clmn[0])
         results.append(int(clmn[1]))
 
+# Find the total amount of months represented in data set
+totalmon = len(months)
+
+# Find the net total revenues/losses for this dataset
+netresult = sum(results)
+
 # Calculate data for monthly differences in profit/loss,...
 # https://www.geeksforgeeks.org/python-calculate-difference-between-adjacent-elements-in-given-list/
 changes = [results[x+1] - results[x] for x in range(len(results)-1)]
@@ -34,24 +40,12 @@ for x in range(len(months)):
         maxgain = results[x]
         dategain = months[x]
 
-# Find the total amount of months represented in data set
-totalmon = len(months)
-
-# Find the net total revenues/losses for this dataset
-netresult = sum(results)
-        
+# Output results into terminal
 print("Financial Analysis")
 print("----------------------------------------------------")
-# Find the total months included in the dataset
 print(f"The dataset covers a total of {totalmon} months.")
-
-# What's the net total revenue/loss for the whole period?
 print(f"The net profit/loss for the covered period is ${netresult}.")
-
-# Did the difference calculator work?
 print(f"The average monthly change in profits/losses was ${avg_change}.")
-
-# Did we get the greatest profit and loss, and their dates?
 print(f"Our greatest profit was of ${maxgain} on {dategain}.")
 print(f"Our greatest loss was of ${maxloss} on {dateloss}.")
 
